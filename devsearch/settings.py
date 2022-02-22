f336e2f8ef891060b20f25138f11a99ef4b28e6e
asadbey <88969785+asadbey@users.noreply.github.com>
2022-02-22 21:51:38 +0500

    initial

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%^m5e7h@!+lduv8748h1axopc4fo*1$uhh-of%m(69l_e^lera'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','devsearchuz.herokuapp.com']
 
